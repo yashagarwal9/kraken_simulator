5231c67b5795e9a89d62c96399c99b31b45aaee0
Aaradhya Sawant <sawantaaradhya@gmail.com>
2018-05-19 14:35:40 +0530

    Removed msgs_stack

--- keyboardcontrol/src/keyboard_control.cpp
+++ keyboardcontrol/src/keyboard_control.cpp
@@ -1,11 +1,11 @@
 #include <ros/ros.h>
 #include <iostream>
-#include <msgs_stack/thrusterData6.h>
+#include <kraken_msgs/thrusterData6Thruster.h>
 
 
 using namespace std;
-msgs_stack::thrusterData6 _force_sent;
-void storePresentForceData(const msgs_stack::thrusterData6 force){
+kraken_msgs::thrusterData6Thruster _force_sent;
+void storePresentForceData(const kraken_msgs::thrusterData6Thruster force){
     _force_sent.data[0] = -force.data[0];
     _force_sent.data[1] = -force.data[1];
     _force_sent.data[2] = -force.data[2];
@@ -17,7 +17,7 @@ void storePresentForceData(const msgs_stack::thrusterData6 force){
 int main(int argc, char **argv){
     ros::init(argc, argv, "KeyboardControl_cpp");
     ros::NodeHandle _nh;
-    ros::Publisher _force_pub = _nh.advertise<msgs_stack::thrusterData6>("krakenSimulator_thrusterData6", 10);
+    ros::Publisher _force_pub = _nh.advertise<kraken_msgs::thrusterData6Thruster>("/kraken/control/thruster6pid", 10);
     ros::Subscriber sub1 = _nh.subscribe("Keyboard_Control", 10, storePresentForceData);
     ros::Rate _looprate(25);
 
--- plugins/src/KrakenControl.cpp
+++ plugins/src/KrakenControl.cpp
@@ -1,6 +1,6 @@
 #include<plugins/KrakenControl.h>
 #include<gazebo/common/Assert.hh>
-#include<geometry_msgs/Twist.h>
+#include<geometry_msgs/TwistWithCovarianceStamped.h>
 #include<geometry_msgs/Pose.h>
 #include<iostream>
 using namespace gazebo;
@@ -18,21 +18,62 @@ void KrakenControlPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf){
   GZ_ASSERT(_sdf != NULL, "RECEIVED NULL SDF POINTER");
   ros::init(KrakenControlPlugin::argc, KrakenControlPlugin::argv, "KrakenControlPlugin");
   ros::NodeHandle n;
-  KrakenControlPlugin::DVLPub = n.advertise<geometry_msgs::Twist>("KrakenSimulator/DVL", 1);
+  KrakenControlPlugin::DVLPub = n.advertise<geometry_msgs::TwistWithCovarianceStamped>("/kraken/sensors/DVL", 1);
   KrakenControlPlugin::PosePub = n.advertise<geometry_msgs::Pose>("KrakenSimulator/Pose", 1);
-  _thrust6dataSub = n.subscribe<msgs_stack::thrusterData6>("krakenSimulator_thrusterData6", 10, &KrakenControlPlugin::thrust6Callback, this);
+  KrakenControlPlugin::ImuPub = n.advertise<sensor_msgs::Imu>("/kraken/sensors/IMU", 1);
+  _thrust6dataSub = n.subscribe<kraken_msgs::thrusterData6Thruster>("/kraken/control/thruster6pid", 10, &KrakenControlPlugin::thrust6Callback, this);
+
 }
 
 void KrakenControlPlugin::Init(){
   this->updateConnection = event::Events::ConnectWorldUpdateBegin(std::bind(&KrakenControlPlugin::Update, this));
+
 }
 
 void KrakenControlPlugin::Update(){
- geometry_msgs::Twist DVL_Twist;
- DVL_Twist.linear.x = this->model->GetLink("DVL")->GetRelativeLinearVel()[0];
- DVL_Twist.linear.y = this->model->GetLink("DVL")->GetRelativeLinearVel()[1];
- DVL_Twist.linear.z = this->model->GetLink("DVL")->GetRelativeLinearVel()[2];
+ geometry_msgs::TwistWithCovarianceStamped DVL_Twist;
+ DVL_Twist.twist.twist.linear.x = this->model->GetLink("DVL")->GetRelativeLinearVel()[0];
+ DVL_Twist.twist.twist.linear.y = this->model->GetLink("DVL")->GetRelativeLinearVel()[1];
+ DVL_Twist.twist.twist.linear.z = this->model->GetLink("DVL")->GetRelativeLinearVel()[2];
  DVLPub.publish(DVL_Twist);
+
+
+ //tf::StampedTransform transform;
+ //try{
+     //listener.lookupTransform("base_link", "odom", ros::Time(0), transform);
+ //}
+ //catch(tf::TransformException ex){
+ //ROS_ERROR("%s",ex.what());
+ //ros::Duration(1.0).sleep();
+ //}
+
+ math::Vector3 grav=this->physicsEngine->GetGravity();;
+ //grav.x=0;
+ //grav.y=0;
+ //grav.z=9.81;
+ physics::LinkPtr link = this->model->GetLink("Imu");
+ math::Pose linkFrame = link->GetWorldPose();
+ math::Vector3 tempo = linkFrame.rot.GetInverse().RotateVector(grav);
+
+ //listener.transformVector("/base_link",grav,tempo);
+
+ sensor_msgs::Imu Imu_data;
+ Imu_data.linear_acceleration.x =tempo.x - this->model->GetLink("Imu")->GetRelativeLinearAccel()[0];
+ Imu_data.linear_acceleration.y =tempo.y - this->model->GetLink("Imu")->GetRelativeLinearAccel()[1];
+ Imu_data.linear_acceleration.z =tempo.z - this->model->GetLink("Imu")->GetRelativeLinearAccel()[2];
+
+ Imu_data.angular_velocity.x = this->model->GetLink("Imu")->GetRelativeAngularVel()[0];
+ Imu_data.angular_velocity.y = this->model->GetLink("Imu")->GetRelativeAngularVel()[1];
+ Imu_data.angular_velocity.z = this->model->GetLink("Imu")->GetRelativeAngularVel()[2];
+
+ //tf::Quaternion q(this->model->GetLink("Imu")->GetRelativePose.orientation[0],this->model->GetLink("Imu")->GetRelativePose[1],this->model->GetLink("Imu")->GetRelativePose[2]);
+ Imu_data.orientation.x = this->model->GetLink("Imu")->GetRelativePose().rot.x;
+ Imu_data.orientation.y = this->model->GetLink("Imu")->GetRelativePose().rot.y;
+ Imu_data.orientation.z = this->model->GetLink("Imu")->GetRelativePose().rot.z;
+ Imu_data.orientation.w = this->model->GetLink("Imu")->GetRelativePose().rot.w;
+
+ ImuPub.publish(Imu_data);
+
  geometry_msgs::Pose _pose;
  physics::ModelState modelState(this->model);
  _pose.position.x = modelState.GetPose().pos.x;
@@ -47,8 +88,8 @@ void KrakenControlPlugin::Update(){
 
 }
 
-void KrakenControlPlugin::thrust6Callback(const msgs_stack::thrusterData6::ConstPtr &msg){
-  msgs_stack::thrusterData6::Type thrust;
+void KrakenControlPlugin::thrust6Callback(const kraken_msgs::thrusterData6Thruster::ConstPtr &msg){
+  kraken_msgs::thrusterData6Thruster::Type thrust;
   for(int i = 0; i<6; i++){
     if((msg->data[i] < 50000) && (msg->data[i] >= 0)){
           thrust.data[i] = msg->data[i];
